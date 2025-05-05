ef7266e9e360cbd7cce5db42cf02d8d11c92c8a0
pkgalishnikova <120177604+pkgalishnikova@users.noreply.github.com>
2025-05-05 23:30:44 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // fixes wallet connect dependency issue https://docs.walletconnect.com/web3modal/nextjs/about#extra-configuration
+  // Fixes wallet connect dependency issue
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
-    module.exports = {
-      staticPageGenerationTimeout: 120,
-    }
     return config;
   },
-  
+  staticPageGenerationTimeout: 120,
 };
 
 export default nextConfig;
